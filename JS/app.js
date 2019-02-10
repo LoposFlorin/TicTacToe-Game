363129daf203e99737d4f4c99521c16471a1b44b
Lopos Florin <loposflorin@gmail.com>
2019-02-10 16:59:57 +0200

    add basic ai player

--- JS/app.js
+++ JS/app.js
@@ -29,8 +29,11 @@ function startGame() {
 
 //function will be called every time a click occures
 function turnClick(square) {
-  //log the id (numbers 0-9) of any clicked square
-  turn(square.target.id, huPlayer)
+  if (typeof origBoard[square.target.id] == "number") {
+    //log the id (numbers 0-9) of any clicked square
+    turn(square.target.id, huPlayer)
+    if (!checkTie()) turn(bestSpot(), aiPlayer);
+  }
 };
 
 /*when player clicks on any square,
@@ -38,4 +41,60 @@ the square innertext = huPlayer = "O" */
 function turn(squareId, player) {
   origBoard[squareId] = player;
   document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(origBoard, player)
+  if (gameWon) gameOver(gameWon)
 };
+
+/* find every index that the player has played in; clicked on */
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) =>
+    (e === player) ? a.concat(i) : a, []);
+    let gameWon = null;
+    //loop trough every winCombos arrays
+    for (let [index, win] of winCombos.entries()) {
+      if (win.every(elem => plays.indexOf(elem) > -1)) {
+        gameWon = {index: index, player: player};
+        break;
+      }
+    }
+    return gameWon;
+};
+
+/*in case of win, color wining combo and remove click event*/
+function gameOver(gameWon) {
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+    gameWon.player == huPlayer ? "blue" : "red";
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener('click', turnClick, false);
+  }
+  declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose!")
+};
+
+function declareWinner(who) {
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame").innerText = who;
+}
+
+//any square that is a number will be empty, any square with value 0 and X ar not empty
+function emptySquares() {
+  return origBoard.filter(s => typeof s == "number");
+}
+
+//always play in the first empty square
+function bestSpot() {
+  return emptySquares()[0];
+};
+
+function checkTie() {
+  if (emptySquares().length == 0) {
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "green";
+      cells[i].removeEventListener('click', turnClick, false);
+    }
+    declareWinner("Tie Game!")
+    return true;
+  }
+  return false;
+}
